59805c90af741d72a3b44b802b41469b5158a6e5
iP <ipzaur@gmail.com>
2019-03-24 21:57:37 +0400

    begin

---
+++ frame.lua
@@ -0,0 +1,110 @@
+local maxWidth = 150
+local noticeWidth = 220
+
+local FONT_ROBOTO = "Interface\\AddOns\\iPMythicTimer\\RobotoCondensed-Light.ttf"
+local backdrop = {
+    bgFile   = "Interface\\TutorialFrame\\TutorialFrameBackground",
+    edgeFile = nil,--"Interface\\Tooltips\\UI-Tooltip-Border",
+    tile     = true,
+    tileSize = 32,
+}
+local affixSize = {
+    width  = 20,
+    height = 20,
+}
+
+local function StartDragging(self, button)
+    self:StartMoving()
+    self.isMoving = true
+end
+
+local function StopDragging(self, button)
+    self:StopMovingOrSizing()
+    self.isMoving = false
+end
+
+
+-- Main Frame
+fIPMT = CreateFrame("Frame", "IPKTMain", UIParent)
+fIPMT:SetSize(140, 80)
+fIPMT:SetPoint("CENTER", 0, 0)
+fIPMT:SetBackdrop(backdrop)
+fIPMT:EnableMouse(true)
+fIPMT:RegisterForDrag("LeftButton")
+fIPMT:SetScript("OnDragStart", StartDragging)
+fIPMT:SetScript("OnDragStop", StopDragging)
+fIPMT:SetMovable(true)
+fIPMT:SetScript("OnEvent", function(self, event, ...)
+    IPMythicTimer:OnEvent(self, event, ...)
+end)
+fIPMT:Hide()
+
+-- Key Level
+fIPMT.level = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.level:SetPoint("LEFT", fIPMT, "TOPLEFT", 6, -16)
+fIPMT.level:SetJustifyH("LEFT")
+fIPMT.level:SetSize(30, 20)
+fIPMT.level:SetFont(FONT_ROBOTO, 20)
+fIPMT.level:SetTextColor(1, 1, 1)
+fIPMT.level:SetText("0")
+
+-- Key Level+
+fIPMT.plusLevel = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.plusLevel:SetPoint("LEFT", fIPMT, "TOPLEFT", 30, -16)
+fIPMT.plusLevel:SetJustifyH("LEFT")
+fIPMT.plusLevel:SetSize(30, 20)
+fIPMT.plusLevel:SetFont(FONT_ROBOTO, 13)
+fIPMT.plusLevel:SetTextColor(0.8, 0.8, 0.8)
+fIPMT.plusLevel:SetText("0")
+
+-- Timer
+fIPMT.timer = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.timer:SetPoint("LEFT", fIPMT, "TOPLEFT", 6, -40)
+fIPMT.timer:SetJustifyH("LEFT")
+fIPMT.timer:SetSize(50, 18)
+fIPMT.timer:SetFont(FONT_ROBOTO, 12)
+fIPMT.timer:SetTextColor(1, 1, 1)
+fIPMT.timer:SetText("00:00")
+-- Timer+
+fIPMT.plusTimer = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.plusTimer:SetPoint("LEFT", fIPMT, "TOPLEFT", 50, -40)
+fIPMT.plusTimer:SetJustifyH("LEFT")
+fIPMT.plusTimer:SetSize(50, 18)
+fIPMT.plusTimer:SetFont(FONT_ROBOTO, 12)
+fIPMT.plusTimer:SetTextColor(0.8, 0.8, 0.8)
+fIPMT.plusTimer:SetText("00:00")
+-- Death Timer
+fIPMT.deathTimer = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.deathTimer:SetPoint("RIGHT", fIPMT, "TOPRIGHT", -4, -40)
+fIPMT.deathTimer:SetJustifyH("RIGHT")
+fIPMT.deathTimer:SetSize(60, 18)
+fIPMT.deathTimer:SetFont(FONT_ROBOTO, 12)
+fIPMT.deathTimer:SetTextColor(0.6, 0.2, 0.2)
+fIPMT.deathTimer:SetText("00:00")
+fIPMT.deathTimer:Hide()
+
+-- Progress
+fIPMT.progress = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.progress:SetPoint("LEFT", fIPMT, "TOPLEFT", 6, -66)
+fIPMT.progress:SetJustifyH("LEFT")
+fIPMT.progress:SetSize(90, 30)
+fIPMT.progress:SetFont(FONT_ROBOTO, 22)
+fIPMT.progress:SetTextColor(1, 1, 1)
+fIPMT.progress:SetText("0%")
+-- Bosses
+fIPMT.bosses = fIPMT:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
+fIPMT.bosses:SetPoint("RIGHT", fIPMT, "TOPRIGHT", -4, -66)
+fIPMT.bosses:SetJustifyH("RIGHT")
+fIPMT.bosses:SetSize(50, 30)
+fIPMT.bosses:SetFont(FONT_ROBOTO, 22)
+fIPMT.bosses:SetTextColor(0.8, 0.8, 0.8)
+fIPMT.bosses:SetText("0/0")
+
+fIPMT.affix = {}
+for f = 1,4 do
+    local right = (-1) * (affixSize.width + 1) * (f-1)
+    fIPMT.affix[f] = CreateFrame("Frame", nil, fIPMT, "ScenarioChallengeModeAffixTemplate")
+    fIPMT.affix[f]:SetSize(affixSize.width, affixSize.height)
+    fIPMT.affix[f].Portrait:SetSize(affixSize.width - 2, affixSize.height - 2)
+    fIPMT.affix[f]:SetPoint("RIGHT", fIPMT, "TOPRIGHT", right - 4, -16)
+end
---
+++ main.lua
@@ -0,0 +1,156 @@
+﻿local BOSS_CRITERIA = 165
+local REAPING = 117
+
+IPMythicTimer = {}
+
+local dungeon = {
+    bosses  = {
+        count  = 0,
+        killed = 0,
+    },
+    affixes = {},
+    isReaping = false,
+    level   = 0,
+}
+local timeCoef = {0.8, 0.6}
+
+local function UpdateDeath()
+    local deathes, timeLost = C_ChallengeMode.GetDeathCount()
+    if deathes > 0 then
+        fIPMT.deathTimer:SetText("-" .. GetTimeStringFromSeconds(timeLost, false, true) .. " [" .. deathes .. "]")
+        fIPMT.deathTimer:Show()
+    else
+        fIPMT.deathTimer:Hide()
+    end
+end
+
+local function UpdateCriteria()
+    local stageName, stageDescription, numCriteria, _, _, _, _, numSpells, spellInfo, weightedProgress, _, widgetSetID = C_Scenario.GetStepInfo()
+
+    dungeon.bosses.count  = 0
+    dungeon.bosses.killed = 0
+
+    for c = 1, numCriteria do
+        local criteriaString, criteriaType, _, quantity, totalQuantity, _, _, quantityString, _, _, _, _, isWeightedProgress = C_Scenario.GetCriteriaInfo(c)
+        if criteriaType == BOSS_CRITERIA then
+            dungeon.bosses.count = dungeon.bosses.count + 1
+            if (quantity > 0) then 
+                dungeon.bosses.killed = dungeon.bosses.killed + 1
+            end
+        elseif isWeightedProgress then
+            local progress = tonumber(strsub(quantityString, 1, -2)) / totalQuantity * 100
+            progress = math.min(100, progress)
+            if (progress % 20 > 13) then
+                fIPMT.progress:SetTextColor(1,0,0)
+            elseif (progress % 20 > 17) then
+                fIPMT.progress:SetTextColor(1,1,0)
+            else
+                fIPMT.progress:SetTextColor(1,1,1)
+            end
+            fIPMT.progress:SetFormattedText("%.2f%%", progress)
+        end
+    end
+
+    fIPMT.bosses:SetText(dungeon.bosses.killed .. "/" .. dungeon.bosses.count)
+end
+
+local function UpdateTime(block, elapsedTime)
+    local plusLevel = 0
+    local plusTimer = 0
+    local r = 0
+    local g = 0
+    local b = 0
+    if elapsedTime < block.timeLimit then
+        for level = 2,1,-1 do
+            local timeLimit = timeCoef[level]*block.timeLimit
+            if elapsedTime < timeLimit then
+                plusLevel = level
+                plusTimer = timeLimit - elapsedTime
+                break
+            end
+        end
+        fIPMT.timer:SetText(GetTimeStringFromSeconds(block.timeLimit - elapsedTime, false, true))
+        fIPMT.timer:SetTextColor(0, 1, 0)
+        if plusTimer > 0 then
+            fIPMT.plusTimer:SetText(GetTimeStringFromSeconds(plusTimer, false, true))
+            fIPMT.plusTimer:Show()
+            g = 1
+            if (plusLevel < 2) then
+                r = 1
+            end
+        else
+            fIPMT.plusTimer:Hide()
+            r = 1
+            g = 1
+            b = 1
+        end
+        plusLevel = "+" .. plusLevel+1
+    else
+        plusLevel = "-1"
+        fIPMT.timer:SetText(GetTimeStringFromSeconds(elapsedTime - block.timeLimit, false, true))
+        fIPMT.plusTimer:Hide()
+        r = 1
+    end
+    fIPMT.timer:SetTextColor(r, g, b)
+    fIPMT.plusLevel:SetText(plusLevel)
+end
+
+local function ShowFrame()
+    local block = ScenarioChallengeModeBlock
+    local level, affixes, wasEnergized = C_ChallengeMode.GetActiveKeystoneInfo()
+
+    dungeon.level = level
+    dungeon.affixes = affixes
+    dungeon.isReaping = false
+
+    fIPMT.level:SetText(dungeon.level)
+    local count = #dungeon.affixes
+    for i,affix in pairs(dungeon.affixes) do
+        local _, _, filedataid = C_ChallengeMode.GetAffixInfo(affix);
+        local iconNum = count - i + 1
+        SetPortraitToTexture(fIPMT.affix[iconNum].Portrait, filedataid);
+        fIPMT.affix[iconNum]:Show()
+
+        if affix == REAPING then
+            dungeon.isReaping = true
+        end
+    end
+    for a = count+1,4 do
+        fIPMT.affix[a]:Hide()
+    end
+    UpdateDeath()
+    UpdateCriteria()
+    fIPMT:Show()
+    ObjectiveTrackerFrame:Hide()
+end
+
+local function StopTimer()
+    fIPMT:Hide()
+    ObjectiveTrackerFrame:Show()
+end
+
+hooksecurefunc("Scenario_ChallengeMode_UpdateTime", UpdateTime)
+hooksecurefunc("Scenario_ChallengeMode_ShowBlock", ShowFrame)
+
+function IPMythicTimer:OnEvent(self, event, ...)
+    if (event == "CHALLENGE_MODE_DEATH_COUNT_UPDATED") then
+        UpdateDeath()
+    elseif (event == "SCENARIO_CRITERIA_UPDATE") then
+        UpdateCriteria()
+    elseif (event == "CHALLENGE_MODE_COMPLETED") then
+        StopTimer()
+    elseif (event == "PLAYER_ENTERING_WORLD") then
+        local inInstance, instanceType = IsInInstance()
+        if not (inInstance and instanceType == "party") then
+            StopTimer()
+        end
+    end
+end
+
+function IPMythicTimer:StartAddon()
+    fIPMT:RegisterEvent("CHALLENGE_MODE_DEATH_COUNT_UPDATED")
+    fIPMT:RegisterEvent("CHALLENGE_MODE_COMPLETED")
+    fIPMT:RegisterEvent("SCENARIO_CRITERIA_UPDATE")
+    fIPMT:RegisterEvent("PLAYER_ENTERING_WORLD")
+    --challengeMapID = C_ChallengeMode.GetActiveChallengeMapID()
+end
