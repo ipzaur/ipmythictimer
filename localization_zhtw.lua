5676977427f831cba0df8116889d18736bb9aa45
iP <ipzaur@gmail.com>
2019-05-19 19:44:13 +0400

    zhTW localization added

---
+++ localization_zhtw.lua
@@ -0,0 +1,10 @@
+if GetLocale() ~= "zhTW" then return end
+
+local AddonName, Addon = ...
+
+Addon.localization.CLOSE     = "關閉"
+Addon.localization.OPTIONS   = "選項"
+Addon.localization.OPACITY   = "背景透明度"
+Addon.localization.RESTORE   = "恢復"
+Addon.localization.SCALE     = "縮放"
+Addon.localization.STARTINFO = "iP Mythic Timer已載入。輸入 /ipmt 開啟選項。"
