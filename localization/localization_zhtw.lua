143b8bcda72a01a1646785c46710761b5ca603b4
BlueNightSky <afan1128@hotmail.com>
2020-04-04 19:46:17 +0800

    Update zhTW

--- localization/localization_zhtw.lua
+++ localization/localization_zhtw.lua
@@ -13,18 +13,18 @@ Addon.localization.CCAPTION   = "主框架:\n" ..
                                 "元素:\n" ..
                                 "    滑鼠左鍵 - 移動\n" ..
                                 "    滑鼠右鍵 - 字體大小\n" ..
-                                "    MMB (请点击) - 切换可见性"
+                                "    MMB (請點擊) - 切換可見性"
 Addon.localization.DAMAGE     = "傷害"
 Addon.localization.DEATHCOUNT = "死亡人數"
 Addon.localization.DEATHSHOW  = "點擊查看詳細訊息"
 Addon.localization.DEATHTIME  = "損失時間"
-Addon.localization.DIRECTION  = "进度变化"
+Addon.localization.DIRECTION  = "進度變化"
 Addon.localization.DIRECTIONS = {
-    [1] = "上升 (0% -> 100%)",
+    [1] = "升序 (0% -> 100%)",
     [2] = "降序 (100% -> 0%)",
 }
 Addon.localization.DTHCAPTION = "死亡紀錄"
-Addon.localization.DUNGENAME  = "地牢名稱"
+Addon.localization.DUNGENAME  = "地城名稱"
 Addon.localization.FONT       = "字型"
 Addon.localization.FONTSIZE   = "字體大小"
 Addon.localization.MAPBUT     = "滑鼠左鍵（單擊）- 切換選項\n" ..
@@ -35,9 +35,9 @@ Addon.localization.OPTIONS    = "選項"
 Addon.localization.OPACITY    = "背景透明度"
 Addon.localization.PROGFORMAT = {
     [1] = "百分 (100.00%)",
-    [2] = "力 (300)",
+    [2] = "強制 (300)",
 }
-Addon.localization.PROGRESS   = "进度格式"
+Addon.localization.PROGRESS   = "進度格式"
 Addon.localization.RESTORE    = "恢復"
 Addon.localization.SCALE      = "縮放"
 Addon.localization.SOURCE     = "資源"
@@ -47,20 +47,20 @@ Addon.localization.UNKNOWN    = "未知"
 Addon.localization.WHODIED    = "誰死了"
 
 Addon.localization.HELP = {
-    LEVEL      = "活动密钥级别",
-    PLUSLEVEL  = "密钥将如何随着当前时间升级",
-    TIMER      = "剩下的时间",
-    PLUSTIMER  = "是时候降级关键进度了",
-    DEATHTIMER = "由于死亡而浪费时间",
-    PROGRESS   = "垃圾被杀死",
-    PROGNOSIS  = "杀死小怪后的进展",
-    BOSSES     = "老板被杀",
-    AFFIXES    = "主动词缀",
+    LEVEL      = "啟動鑰石等級",
+    PLUSLEVEL  = "鑰石將如何隨著當前時間升級",
+    TIMER      = "剩餘的時間",
+    PLUSTIMER  = "降鑰石等級進度的時間",
+    DEATHTIMER = "因死亡而浪費的時間",
+    PROGRESS   = "小怪已擊殺",
+    PROGNOSIS  = "在擊殺拉的小怪後的進度",
+    BOSSES     = "首領已擊殺",
+    AFFIXES    = "啟用的詞綴",
 }
 
 Addon.localization.CORRUPTED = {
-    [161124] = "乌尔格斯，勇士噬灭者 (坦克破碎机)",
-    [161241] = "纺虚者玛熙尔 (蜘蛛)",
-    [161243] = "萨姆莱克，混沌唤引者 (恐惧)",
-    [161244] = "腐蚀者之血 (斑点)",
+    [161124] = "『英雄擊破者』爾格羅斯 (坦克殺手)",
+    [161241] = "虛織者瑪希爾 (蜘蛛)",
+    [161243] = "山姆雷克，混沌召喚者 (恐懼)",
+    [161244] = "腐化者之血 (軟泥)",
 }
